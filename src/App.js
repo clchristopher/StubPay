268d69cce9df4cbad7876d61c01f425ab04eb96a
Private <141947876+QrisHub@users.noreply.github.com>
2023-10-25 15:42:29 -0700

    Add a success component for when user submits form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Form from './Components/application/form';
+import Form from './Components/input-form/form';
+// import Success from "./Components/success/success"
 
 function App() {
   return (
     <div className="App">
       <Form/>
+      {/* <Success /> */}
     </div>
   );
 }
